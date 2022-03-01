adc0750c1ae54a6655067d3166f1c7a99026889c
Jp Siegel <jpsiegel8@gmail.com>
2022-03-01 13:33:45 -0500

    Basic bot setup.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const prefix = '-';
+
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+
+client.once('ready', begin);
+client.on('message', message);
+
+client.login(token);
+
+function begin() {
+    console.log('Bot is online!');
+}
+
+function message(message) {
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'points') {
+        message.channel.send('test');
+    }
+}
